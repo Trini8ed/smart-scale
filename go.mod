d2dcf89ebc99055e50dc54842abf2f61afe73d56
nicholin <nicholaslin1997@gmail.com>
2021-08-26 17:02:27 -0700

    LCD display Golang added

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.16
 require (
 	github.com/MichaelS11/go-hx711 v1.0.0 // indirect
 	periph.io/x/cmd v0.0.0-20210529163517-ed0874b8c370 // indirect
+	periph.io/x/conn/v3 v3.6.8 // indirect
 	periph.io/x/d2xx v0.0.2 // indirect
 )
--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/MichaelS11/go-hx711"
+	"periph.io/x/conn/v3/physic"
+	"periph.io/x/conn/v3/spi"
+	"periph.io/x/conn/v3/spi/spireg"
+	"periph.io/x/periph/host"
 )
 
 func main() {
@@ -40,4 +45,31 @@ func main() {
 
 		fmt.Println(data)
 	}
+
+	// Make sure periph is initialized.
+	if _, err := host.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Use spireg SPI port registry to find the first available SPI bus.
+	p, err := spireg.Open("")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer p.Close()
+
+	// Convert the spi.Port into a spi.Conn so it can be used for communication.
+	c, err := p.Connect(physic.MegaHertz, spi.Mode3, 8)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Write 0x10 to the device, and read a byte right after.
+	write := []byte{0x10, 0x00}
+	read := make([]byte, len(write))
+	if err := c.Tx(write, read); err != nil {
+		log.Fatal(err)
+	}
+	// Use read.
+	fmt.Printf("%v\n", read[1:])
 }
