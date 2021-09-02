97627687420a2d2750fa47b9ecbb5e7321913ce9
nicholin <nicholaslin1997@gmail.com>
2021-09-01 22:46:51 -0700

    added adjustscale

--- PIC16F690.go
+++ PIC16F690.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/MichaelS11/go-hx711"
+)
+
 var characterMap = map[rune]byte{
 	'0': 0x30,
 	'1': 0x31,
@@ -15,4 +21,30 @@ var characterMap = map[rune]byte{
 	'b': 0x26,
 	'c': 0x36,
 	'd': 0x46,
+}
+
+func adjustScale() {
+	err := hx711.HostInit()
+	if err != nil {
+		fmt.Println("HostInit error:", err)
+		return
+	}
+
+	hx711, err := hx711.NewHx711("GPIO6", "GPIO5")
+	if err != nil {
+		fmt.Println("NewHx711 error:", err)
+		return
+	}
+
+	// SetGain default is 128
+	// Gain of 128 or 64 is input channel A, gain of 32 is input channel B
+	// hx711.SetGain(128)
+
+	var weight1 float64
+	var weight2 float64
+
+	weight1 = 100
+	weight2 = 200
+
+	hx711.GetAdjustValues(weight1, weight2)
 }
