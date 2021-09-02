bc25ffea98e59f71b264a5fc1571d5a9fbb66dfb
nicholin <nicholaslin1997@gmail.com>
2021-09-01 22:53:18 -0700

    testin

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/MichaelS11/go-hx711"
 	"periph.io/x/conn/v3/physic"
 	"periph.io/x/conn/v3/spi"
 	"periph.io/x/conn/v3/spi/spireg"
@@ -13,42 +14,42 @@ import (
 )
 
 func main() {
-	/*
-		//Digi Scale
-		err := hx711.HostInit()
+
+	//Digi Scale
+	err := hx711.HostInit()
+	if err != nil {
+		fmt.Println("HostInit error:", err)
+		return
+	}
+
+	hx711, err := hx711.NewHx711("GPIO6", "GPIO5")
+	if err != nil {
+		fmt.Println("NewHx711 error:", err)
+		return
+	}
+
+	defer hx711.Shutdown()
+
+	err = hx711.Reset()
+	if err != nil {
+		fmt.Println("Reset error:", err)
+		return
+	}
+
+	var data int
+	for i := 0; i < 3; i++ {
+		time.Sleep(200 * time.Microsecond)
+
+		data, err := hx711.ReadDataRaw()
 		if err != nil {
-			fmt.Println("HostInit error:", err)
-			return
+			fmt.Println("ReadDataRaw error:", err)
+			continue
 		}
 
-		hx711, err := hx711.NewHx711("GPIO6", "GPIO5")
-		if err != nil {
-			fmt.Println("NewHx711 error:", err)
-			return
-		}
+		fmt.Println(data)
+	}
 
-		defer hx711.Shutdown()
-
-		err = hx711.Reset()
-		if err != nil {
-			fmt.Println("Reset error:", err)
-			return
-		}
-
-		var data int
-		for i := 0; i < 3; i++ {
-			time.Sleep(200 * time.Microsecond)
-
-			data, err := hx711.ReadDataRaw()
-			if err != nil {
-				fmt.Println("ReadDataRaw error:", err)
-				continue
-			}
-
-			fmt.Println(data)
-		}
-	*/
-	adjustScale()
+	//adjustScale()
 	/*******************************************************************/
 	//SPI
 	// Make sure periph is initialized.
