8553ecdf9bbc9af7c22d835a3efdde3017570036
nicholin <nicholaslin1997@gmail.com>
2021-09-06 14:36:57 -0700

    final testing

--- main.go
+++ main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"strconv"
 	"time"
 
 	"github.com/MichaelS11/go-hx711"
+	"periph.io/x/conn/v3/physic"
+	"periph.io/x/conn/v3/spi"
+	"periph.io/x/conn/v3/spi/spireg"
+	"periph.io/x/host/v3"
 )
 
 func main() {
@@ -31,7 +37,7 @@ func main() {
 	}
 
 	var data [4]int
-	for i := 0; i < 3; i++ {
+	for i := 0; i < 4; i++ {
 		time.Sleep(200 * time.Microsecond)
 
 		data[i], err = hx711.ReadDataRaw()
@@ -46,84 +52,84 @@ func main() {
 	/*******************************************************************/
 	//SPI
 	// Make sure periph is initialized.
-	/*
-		if _, err := host.Init(); err != nil {
-			fmt.Println("host.Init error:", err)
-			return
-		}
 
-		// Use spireg SPI port registry to find the first available SPI bus.
-		p, err := spireg.Open("")
-		if err != nil {
-			log.Fatal("Open: ", err)
-		}
-		defer p.Close()
+	if _, err := host.Init(); err != nil {
+		fmt.Println("host.Init error:", err)
+		return
+	}
 
-		// the spi.Port into a spi.Conn so it can be used for communication.
-		c, err := p.Connect(physic.KiloHertz, spi.Mode3, 8)
-		if err != nil {
-			log.Fatal("Connect: ", err)
-		}
+	// Use spireg SPI port registry to find the first available SPI bus.
+	p, err := spireg.Open("")
+	if err != nil {
+		log.Fatal("Open: ", err)
+	}
+	defer p.Close()
 
-		/*******************************************************************/
+	// the spi.Port into a spi.Conn so it can be used for communication.
+	c, err := p.Connect(physic.KiloHertz, spi.Mode3, 8)
+	if err != nil {
+		log.Fatal("Connect: ", err)
+	}
+
+	/*******************************************************************/
 	//LCD Screen activate
 	// turns on the display
-	/*
-		displayOn := []byte{0xFE, 0x41}
-		read := make([]byte, len(displayOn))
-		if err != nil {
-			fmt.Println("cannot open LCD device", err)
-			return
-		}
 
-		if err := c.Tx(displayOn, read); err != nil {
-			fmt.Println("LCD Turned On!", err)
-			log.Fatal(err)
-		}
+	displayOn := []byte{0xFE, 0x41}
+	read := make([]byte, len(displayOn))
+	if err != nil {
+		fmt.Println("cannot open LCD device", err)
+		return
+	}
 
-		// Use read.
+	if err := c.Tx(displayOn, read); err != nil {
+		fmt.Println("LCD Turned On!", err)
+		log.Fatal(err)
+	}
 
-		time.Sleep(time.Microsecond * 100)
-		/*******************************************************************/
-	/*
-		clearScreen := []byte{0xFE, 0x51}
-		read2 := make([]byte, len(clearScreen))
-		if err != nil {
-			fmt.Println("cannot open LCD device", err)
-			return
-		}
+	// Use read.
 
-		if err := c.Tx(clearScreen, read2); err != nil {
-			fmt.Println("LCD Turned On!", err)
-			log.Fatal(err)
-		}
-		time.Sleep(time.Microsecond * 100)
+	time.Sleep(time.Microsecond * 100)
+	/*******************************************************************/
 
-		/*******************************************************************/
+	clearScreen := []byte{0xFE, 0x51}
+	read2 := make([]byte, len(clearScreen))
+	if err != nil {
+		fmt.Println("cannot open LCD device", err)
+		return
+	}
+
+	if err := c.Tx(clearScreen, read2); err != nil {
+		fmt.Println("LCD Turned On!", err)
+		log.Fatal(err)
+	}
+	time.Sleep(time.Microsecond * 100)
+
+	/*******************************************************************/
 	//display on screen
-	/*
-		stringNumber := strconv.Itoa(data)
-		runedNumbers := []rune(stringNumber)
 
-		for _, r := range runedNumbers {
-			fmt.Printf("Rune: %v Hex: 0x%x\n", strconv.QuoteRune(r), characterMap[r])
+	stringNumber := strconv.Itoa(data[3])
+	runedNumbers := []rune(stringNumber)
 
-			// display number on LCD screen
-			displaynumber := []byte{characterMap[r]}
-			read3 := make([]byte, len(displaynumber))
-			if err != nil {
-				fmt.Println("cannot display LCD device", err)
-				return
-			}
+	for _, r := range runedNumbers {
+		fmt.Printf("Rune: %v Hex: 0x%x\n", strconv.QuoteRune(r), characterMap[r])
 
-			if err := c.Tx(displaynumber, read3); err != nil {
-				log.Fatal(err)
-				fmt.Println("display number!", err)
-				return
-			}
-			time.Sleep(time.Microsecond * 100)
+		// display number on LCD screen
+		displaynumber := []byte{characterMap[r]}
+		read3 := make([]byte, len(displaynumber))
+		if err != nil {
+			fmt.Println("cannot display LCD device", err)
+			return
 		}
-	*/
+
+		if err := c.Tx(displaynumber, read3); err != nil {
+			log.Fatal(err)
+			fmt.Println("display number!", err)
+			return
+		}
+		time.Sleep(time.Microsecond * 100)
+	}
+
 	//var data int = 400
 	//database(data)
 }
